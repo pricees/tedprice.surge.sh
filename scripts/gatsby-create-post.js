d16bba49f2dad0546cf5866a2070759acb7aecf5
Ted Price <ted.price@centro.net>
2020-01-26 12:15:27 -0500

    Tweak the script

--- scripts/gatsby-create-post.js
+++ scripts/gatsby-create-post.js
@@ -5,8 +5,7 @@ const moment = require('moment');
 const program = require('commander');
 const postsDirName = "content/posts/";
 
-program
-    .parse(process.argv);
+program.parse(process.argv);
 
 if (process.argv.length < 3) {
     console.log('Please enter the title of the article.');
@@ -20,12 +19,12 @@ fs.exists(postsDirName, (exists) => {
     }
 });
 
-const dirName = process.argv[2];
 const currentDate = moment().format();
+const title = process.argv.slice(2).join(' ');
+const dirName = process.argv.slice(2).join("-").toLowerCase();;
 const fileName = `${postsDirName}${currentDate.slice(0,10)}---${dirName}`;
-const title = dirName.split('-');
 
 fs.writeFileSync(`${fileName}.md`,
-    `---\ntitle: "${title.join(' ')}"\ndate: "${currentDate}"\ntemplate: "post"\ndraft: false\nslug: "${dirName.toLowerCase()}"\ncategory: "CATEGORY"\ntags:\n  - TAG\ndescription: "DESCRIPTION"\n---`, (err) => {
+    `---\ntitle: "${title}"\ndate: "${currentDate}"\ntemplate: "post"\ndraft: false\nslug: "${dirName}"\ncategory: "CATEGORY"\ntags:\n  - TAG\ndescription: "DESCRIPTION"\n---`, (err) => {
         if (err) throw err;
     })
